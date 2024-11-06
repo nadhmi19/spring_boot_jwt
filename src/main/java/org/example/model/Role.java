478cad81ab0f9308bef852b6116329473ec0d461
nadhmi <nadhmii.ayari@gmail.com>
2024-11-06 11:46:26 +0100

    adding employee

--- src/main/java/org/example/model/Role.java
+++ src/main/java/org/example/model/Role.java
@@ -4,7 +4,6 @@ package org.example.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,6 +11,8 @@ import java.util.Set;
 @Table(name="roles")
 @Getter
 @Setter
+@SequenceGenerator(name = "user_seq", sequenceName = "user_seq", allocationSize = 1)
+
 public class Role extends EntityBase {
 
     @Enumerated(EnumType.STRING)
